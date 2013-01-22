aeda484080a44d1e3efa315bf1f1596a0598b56a
Rob Griffiths <rob@bytespider.eu>
2013-01-22 21:04:39 +0000

    Initial Commit

---
+++ HotKey/HotKey.h
@@ -0,0 +1,13 @@
+//
+//  HotKey.h
+//  HotKey
+//
+//  Created by Rob Griffiths on 22/01/2013.
+//  Copyright (c) 2013 Rob Griffiths. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HotKey : NSObject
+
+@end
---
+++ HotKey/HotKey.m
@@ -0,0 +1,13 @@
+//
+//  HotKey.m
+//  HotKey
+//
+//  Created by Rob Griffiths on 22/01/2013.
+//  Copyright (c) 2013 Rob Griffiths. All rights reserved.
+//
+
+#import "HotKey.h"
+
+@implementation HotKey
+
+@end
